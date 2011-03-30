dd35dc188e280ea604a904addf07d93ccf38fc8b
Hugo Duncan <hugo@hugoduncan.org>
2011-03-29 22:26:15 -0400

    Fix pallet.stevedore unquote splicing of empty sequences

--- test/pallet/stevedore_test.clj
+++ test/pallet/stevedore_test.clj
@@ -273,16 +273,28 @@ fi"
   (is (= "ls | ls"
          (script (pipe (ls) (ls))))))
 
+(script/defscript xfn [& args])
+(defimpl xfn :default [& args]
+  ("xfn" ~args))
+
 (deftest unquote-splicing-test
   (is (= "a b c" (script ~@["a" "b" "c"])))
+  (is (= "x" (script x ~@[])))
+  (is (= "x" (script (x ~@[]))))
   (let [x ["a" "b" "c"]]
     (is (= "a b c" (script ~@x))))
+  (let [x []]
+    (is (= "x" (script x ~@x))))
   (let [x nil]
     (is (= "" (script ~@x))))
   (let [x []]
     (is (= "" (script ~@x))))
   (let [fx (fn [] ["a" "b" "c"])]
-    (is (= "a b c" (script ~@(fx))))))
+    (is (= "a b c" (script ~@(fx)))))
+  (let [x nil]
+    (is (= "xfn null" (script (xfn ~@x)))))
+  (let [x [:a 1]]
+    (is (= "xfn a 1" (script (xfn ~@x))))))
 
 (test-utils/with-console-logging-threshold :error
   (script/defscript x [a])
