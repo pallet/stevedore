7813c0c17318f0668753866ad4513c232552d257
Hugo Duncan <hugo@hugoduncan.org>
2013-02-13 16:38:09 -0500

    Add project data to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject com.palletops/stevedore "0.8.0-SNAPSHOT"
+  :description "Embeds shell script in clojure"
+  :url "http://palletops.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:pallet/stevedore.git"}
+
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.0"]
                  [org.cloudhoist/pallet-common "0.3.1"]]
