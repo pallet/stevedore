daee91b3cb3ac9a05a7c2dc386a2a5762db7bd53
Hugo Duncan <hugo@hugoduncan.org>
2013-02-13 16:20:10 -0500

    Add release script

--- release.sh
+++ release.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-# start the release
+# release stevedore
 
-if [[ $# -lt 2 ]]; then
-  echo "usage: $(basename $0) previous-version new-version" >&2
+if [[ $# -lt 3 ]]; then
+  echo "usage: $(basename $0) previous-version new-version next-version" >&2
   exit 1
 fi
 
 previous_version=$1
 version=$2
+next_version=$3
 
-$(dirname $0)/start-release.sh $previous_version $version
-$(dirname $0)/finish-release.sh $version
+echo ""
+echo "Start release of $version, previous version is $previous_version"
+echo ""
+echo ""
+
+lein do clean, with-profile default:+clojure-1.5.0 test && \
+git flow release start $version || exit 1
+
+lein with-profile +release set-version ${version} :previous-version ${previous_version} \
+  || { echo "set version failed" >2 ; exit 1; }
+
+echo ""
+echo ""
+echo "Changes since $previous_version"
+git --no-pager log --pretty=changelog stevedore-$previous_version..
+echo ""
+echo ""
+echo "Now edit project.clj, ReleaseNotes and README"
+
+$EDITOR project.clj
+$EDITOR ReleaseNotes.md
+$EDITOR README.md
+
+echo -n "commiting project.clj, release notes and readme.  enter to continue:" \
+&& read x \
+&& git add project.clj ReleaseNotes.md README.md \
+&& git commit -m "Updated project.clj, release notes and readme for $version" \
+&& echo -n "Peform release.  enter to continue:" && read x \
+&& lein do clean, with-profile default:+clojure-1.5.0 test, deploy clojars \
+&& git flow release finish $version \
+&& echo "Now push to github. Don't forget the tags!" \
+&& lein with-profile +doc doc \
+&& lein with-profile +release set-version ${next_version} \
+&& git add project.clj \
+&& git commit -m "Updated version for next release cycle"
