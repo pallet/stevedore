f0edc64b224192866d0cdb419a9a6af4bb15d980
Hugo Duncan <hugo@hugoduncan.org>
2013-02-13 16:09:23 -0500

    Update groupID to com.palletops

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.cloudhoist/stevedore "0.8.0-SNAPSHOT"
+(defproject com.palletops/stevedore "0.8.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.0"]
-                 [org.cloudhoist/pallet-common "0.3.1-SNAPSHOT"]]
+                 [org.cloudhoist/pallet-common "0.3.1"]]
   :profiles
   {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]}})
