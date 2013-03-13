b134e6fd92f6be3104f6571179af932d5adb759c
Hugo Duncan <hugo@hugoduncan.org>
2013-03-13 13:52:26 -0400

    Update to pallet-common 0.4.0

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.0"]
-                 [org.cloudhoist/pallet-common "0.3.1"]]
+                 [com.palletops/pallet-common "0.4.0"]]
   :profiles
   {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]}})
