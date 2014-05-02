9ca494f905cb3f5739becf9f08294b319afa1f5e
Hugo Duncan <hugo@hugoduncan.org>
2014-05-02 16:07:07 -0400

    Enable travis and release configuration

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:pallet/stevedore.git"}
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.0"]
-                 [com.palletops/pallet-common "0.4.0"]]
-  :profiles
-  {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]}})
+  :dependencies [[org.clojure/tools.logging "0.2.0"
+                  :exclusions [org.clojure/clojure]]
+                 [com.palletops/pallet-common "0.4.0"
+                  :exclusions [org.clojure/clojure]]])
